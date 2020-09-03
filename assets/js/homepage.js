727b7b3293d6bf307af48ca086cad4aba7a3baea
forsythrl <tarrantrl@utexas.edu>
2020-09-02 21:30:57 -0500

    add user parameter to get user repos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+    // format github api url
+    var apiUrl = "https://api.github.com/users/" + user+"/repos";
+    // make a request to the url
+    // fetch happens asynchronously to not slow everything down - AJAX asynch javascript and XML
+    fetch(apiUrl).then(function(response){
+        // response object has methods json() and text() for non json data
+        // json() returns another Promise object with another then() method
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
 };
   
-getUserRepos();
+getUserRepos("facebook");
