9ea1d92a6989fc07074291a47e92b009b18e6b80
forsythrl <tarrantrl@utexas.edu>
2020-09-05 18:14:02 -0500

    add message if there are more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo){
     // repo is owner/repo name
@@ -8,6 +9,10 @@ var getRepoIssues = function(repo){
         if (response.ok){
             response.json().then(function(data){
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")){
+                    displayWarning(data);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -45,5 +50,16 @@ var displayIssues = function(issues){
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo){
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+    linkEl.setAttribute("target", "_blank");
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 getRepoIssues("facebook/react");
