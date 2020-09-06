c605d2c3519e3272d3dea6ec541cf9ebe1d45fdd
forsythrl <tarrantrl@utexas.edu>
2020-09-05 21:42:35 -0500

    add featured language search buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format github api url
@@ -73,5 +74,30 @@ var displayRepos = function(repos, searchTerm){
         repoContainerEl.appendChild(repoEl);
     }
 }
+
+var getFeaturedRepos = function(language){
+    var apiURL = `https://api.github.com/search/repositories?q=${language}+is:featured&sort=help-wanted-issues`;
+    fetch(apiURL).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+}
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    if (language){
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+        repoSearchTerm.textContent = "";
+    }
+}
   
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
